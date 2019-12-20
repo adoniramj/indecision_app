ca7f93e75da4638a8dcec113a683285bea1c0eb4
adoniramj <adoniramjvargas@gmail.com>
2019-12-20 18:54:55 -0500

    JSX expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,10 +5,52 @@ console.log('App.js is running');
 // JSX - JavaScript XML
 
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision app'
+  React.createElement(
+    'h1',
+    null,
+    'Adoniram'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
 
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Adoniram Vargas'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'age: 46'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'location: Miami'
+  )
+);
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);

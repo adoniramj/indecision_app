a978b175c472d277014cfa2b2ddcf94290955132
adoniramj <adoniramjvargas@gmail.com>
2019-12-20 11:10:48 -0500

    Babel with dependencies installation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,14 @@
-console.log('App.js is running')
+'use strict';
+
+console.log('App.js is running');
 
 // JSX - JavaScript XML
 
-//var template = <p>This is JSX from app.js!</p>;
-var template = React.createElement("h1", {
-  id: "someid"
-}, "This is JSX code");
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision app'
+);
 
-var appRoot = document.getElementById('app')
-ReactDOM.render(template, appRoot)
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);

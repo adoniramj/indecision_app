433240b7b3353923a0d7893aca2870906a806c00
adoniramj <adoniramjvargas@gmail.com>
2020-07-21 10:19:37 -0400

    Completed section 10
    Initial exploration of JSX. JSX needs a parent wrapper to render
    properly.
    
    Removed public/scripts/app.js from git tracking.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 console.log('App.js is running! Is it?')
 
-var template = <p>This is JSX from app.js!</p>;
+var template = (
+    <div>
+        <h1>This is JSX from app.js!</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+)
+
+var templateTwo = (
+    <div>
+        <h1>Josue</h1>
+        <p>Age: 47</p>
+        <p>Location: Miramar</p>
+    </div>
+)
 
 var appRoot = document.getElementById('app')
-ReactDOM.render(template,appRoot)
+ReactDOM.render(templateTwo,appRoot)

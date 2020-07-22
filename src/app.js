825d0c4258b76ac27add7037ee4f9126280cc94e
adoniramj <adoniramjvargas@gmail.com>
2020-07-22 09:36:02 -0400

    Completed sections 11 and 12
    Use {expressions} to execute JS expressions inside JSX. If statements
    are not expressions.
    
    Conditional rendering inside JSX use functions, ternary operators,
    and and operators.  React ignores booleans and nulls.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 console.log('App.js is running! Is it?')
 
+var app = {
+    title : 'Indecision App',
+    subtitle : 'Put your life in the hands of a computer!',
+    options : ['one', 'two']
+}
+
 var template = (
     <div>
-        <h1>This is JSX from app.js!</h1>
-        <p>This is some info</p>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {app.options.length > 0 ? 'Here are your options' : 'No options'}
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -11,13 +18,30 @@ var template = (
     </div>
 )
 
+var userName = "Mike"
+var age = 4
+var userLocation = 'Miramar'
+
+var user = {
+    name : 'Adoniram',
+    age : 47,
+    location : 'Miramar'
+}
+
+// var user = {}
+
+function getLocation(location) {
+    if(location){
+        return <p>Location: {location}</p>
+    } 
+}
 var templateTwo = (
     <div>
-        <h1>Josue</h1>
-        <p>Age: 47</p>
-        <p>Location: Miramar</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {user.age >= 18 && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 )
 
 var appRoot = document.getElementById('app')
-ReactDOM.render(templateTwo,appRoot)
+ReactDOM.render(template,appRoot)

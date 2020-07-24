1f21c57bd6b8a02451c3d170ff16b08cebff8498
adoniramj <adoniramjvargas@gmail.com>
2020-07-24 10:04:55 -0400

    Completed sections 18 and 19
    When an event is executed, the event can pass as an argument an object.
    This may or may not be needed. In a form submit, the event object is
    needed to retrieve the values the user inputted.
    
    Reviewed how React handles arrays and how to render them.

--- src/app.js
+++ src/app.js
@@ -3,66 +3,49 @@ console.log('App.js is running! Is it?')
 var app = {
     title : 'Indecision App',
     subtitle : 'Put your life in the hands of a computer!',
-    options : ['one', 'two']
+    options : []
 }
 
-var template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        {app.options.length > 0 ? 'Here are your options' : 'No options'}
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-)
-
-var userName = "Mike"
-var age = 4
-var userLocation = 'Miramar'
-
-var user = {
-    name : 'Adoniram',
-    age : 47,
-    location : 'Miramar'
+const onFormSubmit = (e) => {
+    e.preventDefault()
+    const option = e.target.elements.option.value
+    if(option){
+        app.options.push(option)
+        console.log(app.options)
+        e.target.elements.option.value = ''
+        renderingApp()
+    }
 }
 
-let count = 0
-
-const addOne = () => {
-    count++
-    console.log('addOne', count)
-    renderCounterApp()
+const onRemoveAll = (e)=> {
+    console.log(e)
+    app.options = []
+    renderingApp()
 }
 
-const minusOne = () => {
-    count--
-    console.log('minusOne')
-    renderCounterApp()
-}
-
-const reset = () => {
-    count = 0
-    console.log('reset')
-    renderCounterApp()
-}
-
-const renderCounterApp = () => {
-    const templateTwo = (
+const renderingApp = () => {
+    const template = (
         <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne} className="button">+1</button>
-            <button onClick={minusOne} className="button">-1</button>
-            <button onClick={reset} className="button">Reset</button>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <button onClick={onRemoveAll}>Remove options</button>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type='text' name='option'/>
+                <button>Add option</button>
+            </form>
         </div>
     )
-    ReactDOM.render(templateTwo,appRoot)
+    ReactDOM.render(template,appRoot)
 }
 
 
+const appRoot = document.getElementById('app')
 
-//console.log(templateTwo)
-var appRoot = document.getElementById('app')
 
-renderCounterApp()
+renderingApp()
---
+++ src/playground/counter-example.js
@@ -0,0 +1,46 @@
+let count = 0
+
+const addOne = function(e){
+    console.log(e)
+    console.log(typeof e)
+    console.log(e.type)
+    console.log(arguments)
+    count++
+    renderCounterApp()
+}
+// const addOne = (e) => {
+//     console.log(e)
+//     console.log(e.type)
+//     count++
+//     console.log('addOne', count)
+//     renderCounterApp()
+// }
+
+const minusOne = (e) => {
+    count--
+    console.log('minusOne')
+    renderCounterApp()
+}
+
+const reset = () => {
+    count = 0
+    console.log('reset')
+    renderCounterApp()
+}
+
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne} className="button">+1</button>
+            <button onClick={minusOne} className="button">-1</button>
+            <button onClick={reset} className="button">Reset</button>
+        </div>
+    )
+    ReactDOM.render(templateTwo,appRoot)
+}
+
+
+var appRoot = document.getElementById('app')
+
+renderCounterApp()

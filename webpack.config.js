d34d58adb4810655e946cc89a494dc46952ecde3
adoniramj <adoniramjvargas@gmail.com>
2020-07-31 15:32:08 -0400

    Modified webpack.config.js
    Added the options and plugins to babel-loader.
    
    Deleted .babelrc.

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,17 @@ module.exports = {
         rules : [{
             loader : 'babel-loader',
             test : /\.js$/,
-            exclude : /node_modules/
+            exclude : /node_modules/,
+            options : {
+                presets: ["env", "react"],
+                plugins : ["transform-class-properties"]
+            }
         }]
     },
     devtool : 'cheap-module-eval-source-map',
     devServer : {
         contentBase : path.join(__dirname,'public')
     }
+   
 
 }
